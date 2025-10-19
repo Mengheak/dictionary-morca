4502466144837f0c8cf47c4f50b1fd7f305d3bdd
unknown <mengheak088@gmail.com>
2025-10-19 21:36:07 +0700

    F

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 const MainHeader: React.FC = () => {
   return (
-    <header className='relative text-center top-0 left-0 right-0 font-bold py-5 flex flex-col md:flex-row gap-5 items-center'>
+    <header className='relative text-center justify-center top-0 left-0 right-0 font-bold py-5 flex flex-col md:flex-row gap-5 items-center'>
       <img
         src="/logo.png"
         alt="Sleepy cat"
@@ -9,7 +9,7 @@ const MainHeader: React.FC = () => {
         className='w-[150px]'
       />
       <span className='uppercase text-yellow-800'>
-        <h1 className='text-3xl font-bold'>
+        <h1 className='text-2xl font-bold'>
           ក្រសួងធម្មកា​ និង​ សាសនា
         </h1>
         Ministry of Cult and Religion
